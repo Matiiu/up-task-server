e802c8c780698f81ac7b740a8bdf4e9c880c2cb5
Matiiu <mateotaveraramirez@gmail.com>
2024-08-14 22:57:35 -0500

    refactor: Remove unused express-validator import in ProjectRoutes

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body, param } from 'express-validator';
+import { body } from 'express-validator';
 import ProjectController from '../controllers/ProjectController';
 import { ProjectErrorMsg, TaskErrorMsg } from '../data/MessagesAPI';
 import handleInputErrors from '../middleware/validation';
