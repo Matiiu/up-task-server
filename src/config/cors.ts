eb6a1b87daf74ec093c0b1d6accea03048f2e641
Matiiu <mateotaveraramirez@gmail.com>
2024-11-08 05:00:15 -0500

    feat: Add logging for CORS origin in cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,9 +1,10 @@
 import { CorsOptions } from 'cors';
+import colors from 'colors';
 
 const corsConfig: CorsOptions = {
 	origin: (origin, callback) => {
 		const whiteList = [process.env.FRONTEND_URL];
-
+		console.log(colors.bgBlue.bold(`The origin is ${origin}`));
 		if (process.argv.includes('--api')) {
 			whiteList.push(undefined);
 		}
